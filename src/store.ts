3e4f6cbdfc39c229a414819aadc3a44892893bff
Antonio Torres Ochoa <antoniotrrs@iMac-de-Antonio.local>
2025-09-05 18:13:31 -0600

    store orders

--- src/store.ts
+++ src/store.ts
@@ -4,17 +4,54 @@ import { Product } from '@/prisma/generated/prisma'
 
 interface Store {
     order: OrderItem[]
-    addToOrder: (product: Product) => void
+    addToOrder: (product: Product) => void,
+    increaseQuantity: (id: Product['id']) => void,
+    decreaseQuantity: (id: Product['id']) => void,
+    removeItem: (id: Product['id']) => void,
 }
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
     order: [],
     addToOrder: (product) => {
+        let order: OrderItem[] = []
+
+        if (get().order.find(item => item.id === product.id)) {
+            order = get().order.map(item => item.id === product.id ? {
+                ...item,
+                quantity: item.quantity + 1,
+                subtotal: item.price * (item.quantity + 1)
+            } : item)
+        } else {
+            order = [...get().order, {
+                ...product,
+                subtotal: 1 * product.price,
+                quantity: 1
+            }]
+        }
+
+        set(() => ({ order }))
+    },
+    increaseQuantity: (id) => {
         set((state) => ({
-            order: [...state.order, {
-                ...product, 
-                subtotal: 1 * product.price, 
-                quantity: 1}]
+            order: state.order.map(item => item.id === id ? {
+                ...item,
+                quantity: item.quantity + 1,
+                subtotal: item.price * (item.quantity + 1)
+            } : item)
+        }))
+    },
+    decreaseQuantity: (id) => {
+        set((state) => ({
+            order: state.order.map(item => item.id === id ? {
+                ...item,
+                quantity: item.quantity - 1,
+                subtotal: item.price * (item.quantity - 1)
+            } : item)
+        }))
+    },
+    removeItem: (id) => {
+        set((state) => ({
+            order: state.order.filter(item => item.id !== id)
         }))
     }
 }))
