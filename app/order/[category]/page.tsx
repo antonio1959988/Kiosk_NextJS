9d86dbafa8301084ce404bfbb8f88ec06ba10c4b
Antonio Torres Ochoa <antoniotrrs@iMac-de-Antonio.local>
2025-12-16 13:09:52 -0600

    feat: Add products

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,12 +1,39 @@
+import ProductCard from "@/components/products/ProductCard";
+import { prisma } from "@/src/lib/prisma";
 
 type OrderPageProps = {
-  params: { 
+  params: {
     category: string
-   }
+  }
 }
 
-export default function OrderPage({ params } : OrderPageProps) {
+
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category
+      }
+    }
+  });
+
+  return products;
+}
+
+export default async function OrderPage({ params }: OrderPageProps) {
+  const products = await getProducts(params.category)
+
   return (
-    <div>{params.category}</div>
+    <>
+      <h1 className="text-2xl my-10">Elige y personaliza tu pedido a continuación</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map(product => (
+          <ProductCard 
+            key={product.id} 
+            product={product}
+          />
+        ))}
+      </div>
+    </>
   )
 }
--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,14 +1,19 @@
+"use client";
 import { Category } from "@/src/generated/prisma/client"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 
 type CategoryIconProps = {
     category: Category
 }
 
 export default function CategoryIcon({ category } : CategoryIconProps) {
+
+  const params = useParams<{ category: string }>();
+
   return (
-    <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+    <div className={`${category.slug === params.category ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
       <Image
         src={`/icon_${category.slug}.svg`}
         alt={`Imagen de la Categoria ${category.name}`}
