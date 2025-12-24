35665888b2387476a5e85456bef30b6f2d9e8859
Antonio Torres Ochoa <antoniotrrs@iMac-de-Antonio.local>
2025-12-24 13:30:22 -0600

    new order link

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -5,6 +5,7 @@ const adminNavigation = [
     {url: '/admin/orders', text: 'Ordenes', blank: false},
     {url: '/admin/products', text: 'Productos', blank: false},
     {url: '/order/cafe', text: 'Ver Quiosco', blank: true},
+    {url: '/orders', text: 'Ver Ordenes Listas', blank: true},
 ]
 
 export default function AdminSidebar() {
